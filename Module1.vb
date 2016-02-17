3fe2289cc793a483f9e99736bf00b5504e1fe170
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-02-17 17:29:27 +0400

    Add my finding a graph of the volume of a cylindrical horseshoe from the angle

---
+++ Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    Sub Main()
+        Dim n, b, v, r, a, u As Double
+        Randomize()
+        Console.WriteLine("Введите значение a")
+        a = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Введите значение b")
+        b = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Введите значение r")
+        r = Convert.ToInt32(Console.ReadLine())
+        'построения графика зависимости объема V от угла u, если a, b и r известны, а и u изменяется 
+        'в диапазоне от u(1) до u(2) с шагом погрешности u равным 0,125' 
+        For n = 1 To 2 Step 0.125
+            v = n / 3 * b * (a * (3 * (r ^ 2) - (a ^ 2)) + 3 * (r ^ 2) * (b - r) * u * n / 180)
+            Console.Write("Значения n " & n) 'шаг графика зависимости объема 
+            Console.WriteLine("     " & "Значения v " & v) 'значение объема на шаге
+        Next n
+        Console.ReadLine()
+    End Sub
+End Module
+
---
+++ READ_ME_ENG.md
@@ -0,0 +1,6 @@
+﻿This program is in the programming language VB,
+demonstrates the formula for calculating the volume of a cylindrical horseshoe using VB language,
+where to plot the dependence of the volume V on the angle u, if a, b and r are known, a and u change
+in the range from u (1) to u (2) with an error step u equal to 0.125
+The main Module1 module contains a single main function.
+inside the entry point Sub Main () - fully calculating the volume of the cylindrical horseshoe
